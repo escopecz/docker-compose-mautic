1dc3c78c8830d35040a044b02fa8a9b6a993623b
John Linhart <jan@linhart.email>
2024-07-05 18:13:05 +0200

    Getting back to docker

--- setup-dc.sh
+++ setup-dc.sh
@@ -2,8 +2,6 @@ docker compose build
 docker compose up -d mautic_web
 
 echo "## Wait for basic-mautic_web-1 container to be fully running"
-
-echo "## Wait for basic-mautic_web-1 container to be fully running"
 while ! docker exec basic-mautic_web-1 sh -c 'echo "Container is running"'; do
     echo "### Waiting for basic-mautic_web-1 to be fully running..."
     sleep 2
@@ -13,11 +11,9 @@ echo "## Check if Mautic is installed"
 if docker compose exec -T mautic_web test -f /var/www/html/config/local.php && docker compose exec -T mautic_web grep -q "site_url" /var/www/html/config/local.php; then
     echo "## Mautic is installed already."
 else
-    docker compose stop mautic_worker # avoiding https://github.com/mautic/docker-mautic/issues/270
-
+    docker stop basic-mautic_worker-1 # avoiding https://github.com/mautic/docker-mautic/issues/270
     echo "## Ensure the worker is stopped before installing Mautic"
-
-    while docker compose ps basic-mautic_worker | grep -q "Up"; do
+    while docker ps -q --filter name=basic-mautic_worker-1 | grep -q .; do
         echo "### Waiting for basic-mautic_worker-1 to stop..."
         sleep 5
     done
