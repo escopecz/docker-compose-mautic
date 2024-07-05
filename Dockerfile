985c23e2cdb0abf30120b95958d6fb78134d8939
John Linhart <jan@linhart.email>
2024-07-05 14:36:52 +0200

    The global composer is necessary it seems

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 # Build stage:
 FROM mautic/mautic:5.0.3-apache AS build
-WORKDIR /var/www/html
 
 # Install dependencies needed for Composer to run and rebuild assets:
 RUN apt-get update && apt-get install -y git curl npm && rm -rf /var/lib/apt/lists/*
 
+# Install Composer globally:
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
 # Install any Mautic theme or plugin using Composer:
-RUN php vendor/bin/composer require chimpino/theme-air --no-scripts
+RUN cd /var/www/html && \
+    COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000  vendor/bin/composer require chimpino/theme-air --no-scripts --no-interaction
 
 # Production stage:
 FROM mautic/mautic:5.0.3-apache
-WORKDIR /var/www/html
 
 # Copy the built assets and the Mautic installation from the build stage:
 COPY --from=build --chown=www-data:www-data /var/www/html /var/www/html
---
+++ run.sh
@@ -0,0 +1,25 @@
+docker compose build
+docker compose up -d mautic_web
+
+echo "## Wait for basic-mautic_web-1 container to be fully running"
+while ! docker exec basic-mautic_web-1 sh -c 'echo "Container is running"'; do
+echo "### Waiting for basic-mautic_web-1 to be fully running..."
+sleep 2
+done
+
+echo "## Check if Mautic is installed"
+if test -f /var/www/html/config/local.php && grep -q "site_url" /var/www/html/config/local.php; then
+echo "## Mautic is installed already."
+else
+docker stop basic-mautic_worker-1 # avoiding https://github.com/mautic/docker-mautic/issues/270
+echo "## Ensure the worker is stopped before installing Mautic"
+while docker ps -q --filter name=basic-mautic_worker-1 | grep -q .; do
+    echo "### Waiting for basic-mautic_worker-1 to stop..."
+    sleep 5
+done
+echo "## Installing Mautic..."
+docker exec -u www-data -w /var/www/html basic-mautic_web-1 php ./bin/console mautic:install --force --admin_email willchange@mautic.org --admin_password MauticR0cks! http://157.230.80.20:8001
+fi
+
+echo "## Starting all the containers"
+docker compose up -d
