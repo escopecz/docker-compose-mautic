c921ee002011d96034014a0331f22835dc46222e
John Linhart <jan@linhart.email>
2024-07-07 12:01:07 +0200

    Better handling of the worker

--- setup-dc.sh
+++ setup-dc.sh
@@ -12,13 +12,18 @@ echo "## Check if Mautic is installed"
 if docker compose exec -T mautic_web test -f /var/www/html/config/local.php && docker compose exec -T mautic_web grep -q "site_url" /var/www/html/config/local.php; then
     echo "## Mautic is installed already."
 else
-    docker stop basic-mautic_worker-1 # avoiding https://github.com/mautic/docker-mautic/issues/270
-    echo "## Ensure the worker is stopped before installing Mautic"
-    while docker ps -q --filter name=basic-mautic_worker-1 | grep -q .; do
-        echo "### Waiting for basic-mautic_worker-1 to stop..."
-        sleep 5
-    done
-
+    # Check if the container exists and is running
+    if docker ps --filter "name=basic-mautic_worker-1" --filter "status=running" -q | grep -q .; then
+        echo "Stopping basic-mautic_worker-1 to avoid https://github.com/mautic/docker-mautic/issues/270"
+        docker stop basic-mautic_worker-1
+        echo "## Ensure the worker is stopped before installing Mautic"
+        while docker ps -q --filter name=basic-mautic_worker-1 | grep -q .; do
+            echo "### Waiting for basic-mautic_worker-1 to stop..."
+            sleep 2
+        done
+    else
+        echo "Container basic-mautic_worker-1 does not exist or is not running."
+    fi
     echo "## Installing Mautic..."
     docker compose exec -T -u www-data -w /var/www/html mautic_web php ./bin/console mautic:install --force --admin_email {{EMAIL_ADDRESS}} --admin_password {{MAUTIC_PASSWORD}} http://{{IP_ADDRESS}}:{{PORT}}
 fi
